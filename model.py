b422e3d76e012d80a2105900766d36c78bcfd9d5
Namanjeet Singh <51831819+OfficialNMN@users.noreply.github.com>
2022-01-16 01:28:06 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,56 @@
+import os
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt 
+import numpy as np
+from tensorflow.keras.utils.np_utils import to_categorical
+import random,shutil
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dropout,Conv2D,Flatten,Dense, MaxPooling2D, BatchNormalization
+from tensorflow.keras.models import load_model
+
+
+def generator(dir, gen=image.ImageDataGenerator(rescale=1./255), shuffle=True,batch_size=1,target_size=(24,24),class_mode='categorical' ):
+
+    return gen.flow_from_directory(dir,batch_size=batch_size,shuffle=shuffle,color_mode='grayscale',class_mode=class_mode,target_size=target_size)
+
+batch= 32
+target=(24,24)
+train_batch= generator(data_dir,shuffle=True, batch_size=batch,target_size=target)
+valid_batch= generator(data_dir,shuffle=True, batch_size=batch,target_size=target)
+Size_per_epoch= len(train_batch.classes)//batch
+validation_size = len(valid_batch.classes)//batch
+print(Size_per_epoch,validation_size)
+
+
+# img,labels= next(train_batch)
+# print(img.shape)
+
+model = Sequential([
+    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(24,24,1)),
+    MaxPooling2D(pool_size=(1,1)),
+    Conv2D(32,(3,3),activation='relu'),
+    MaxPooling2D(pool_size=(1,1)),
+    #32 convolution filters used each of size 3x3 again
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(1,1)),
+
+    #64 convolution filters used each of size 3x3 choose the best features via pooling
+    
+    #randomly turn neurons on and off to improve convergence
+    Dropout(0.25),
+    #flatten since too many dimensions, we only want a classification output
+    Flatten(),
+    #fully connected to get all relevant data
+    Dense(128, activation='relu'),
+    #one more dropout for convergence' sake :)
+    Dropout(0.5),
+    #output a softmax to squash the matrix into output probabilities
+    Dense(2, activation='softmax')
+])
+
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+
+model.fit_generator(train_batch, validation_data=valid_batch,epochs=15,steps_per_epoch=SPE ,validation_steps=VS)
+
+model.save('models/cnnCat2.h5', overwrite=True)
